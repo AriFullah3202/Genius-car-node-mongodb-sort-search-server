d913dee4b3a0aa9ca5700101d6fe170ee3b7ec67
Arif <marifullaah@gmail.com>
2022-11-06 22:35:56 +0600

    added delete api

--- index.js
+++ index.js
@@ -62,6 +62,12 @@ async function run() {
         res.send(result)
         console.log(result)
     })
+    app.delete('/orders/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await orderCollection.deleteOne(query);
+        res.send(result)
+    })
 
 }
 run().catch(err => { console.log(err) })
